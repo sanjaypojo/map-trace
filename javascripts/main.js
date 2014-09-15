60081fc9093f244393746d679d3e50702577b936
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2014-09-15 13:36:02 +0530

    Integrate mapbox
    Plot the same trucks using map-trace
    Remove unnecessary image styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,6 +21,19 @@ var initialize = function() {
     // To add the marker to the map, call setMap();
     marker.setMap(map);
   }
+
+  // Mapbox map
+  L.mapbox.accessToken = 'pk.eyJ1Ijoic2FuamF5cG9qbyIsImEiOiJYcGVzdDk4In0.lcVPF6zqBpDlEc7Sbwmv3A';
+  var mapBox = L.mapbox.map('mapbox-canvas', 'sanjaypojo.jgl9ojan')
+    .setView([gpsDataObj[0][1],gpsDataObj[0][2]], 6);
+
+  for (var k=0; k<gpsDataObj.length; k++) {
+    marker = L.marker([gpsDataObj[k][1],gpsDataObj[k][2]], {
+      icon: L.mapbox.marker.icon({
+        'marker-color': '#f86767'
+      })
+    }).addTo(mapBox)
+  }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
